e8ea49e2ccd4b6b704bc7f5257a8646307f4c3dd
TiBo <thithip@gmail.com>
2022-09-09 10:35:41 +0200

    feat(adapter): use animateTo on value update with the ValueNotifierAdapter
    Before this update, the new value was directly set to the controller.
    Now, the controller will animate to the new value, with previously set animation parameters (Duration, Curve)
    A default `duration` parameter was added to allow disable the animation if setted to zero

--- lib/adapters/value_notifier_adapter.dart
+++ lib/adapters/value_notifier_adapter.dart
@@ -6,17 +6,22 @@ import '../flutter_animate.dart';
 
 /// Drives an [Animate] animation from a [ValueNotifier]. The value from the
 /// notifier should be in the range `0-1`.
+/// The [Duration] parameter allows to enable or disable the animation, or update the Animation Duration:
+/// - set a `null` [Duration] (or leave it empty) to animate and preserve Animation parameters (default behavior) ;
+/// - set a zero [Duration] (short with `0.ms`) to disable the Animation ;
+/// - set a new [Duration] to update the Animation duration.
 @immutable
 class ValueNotifierAdapter extends Adapter {
-  ValueNotifierAdapter(this.notifier);
+  ValueNotifierAdapter(this.notifier, {this.duration});
 
   final ValueNotifier<double> notifier;
+  final Duration? duration;
 
   @override
   void init(AnimationController controller) {
     controller.value = notifier.value;
     notifier.addListener(() {
-      controller.value = notifier.value;
+      controller.animateTo(notifier.value, duration: duration);
     });
   }
 }
