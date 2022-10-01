deda08ea331c3f1c3cb4ea6b114222a34d19c3a1
Grant Skinner <grant@gskinner.com>
2022-10-01 11:09:02 -0600

    Add moveX/Y and slideX/Y methods

--- lib/effects/move_effect.dart
+++ lib/effects/move_effect.dart
@@ -4,7 +4,7 @@ import '../flutter_animate.dart';
 
 /// Effect that moves the target (via [Transform.translate]) between the specified begin and end offsets.
 /// Defaults to `begin=Offset(0, -16), end=Offset.zero`.
-/// 
+///
 /// To specify offsets relative to the target's size, use [SlideEffect].
 @immutable
 class MoveEffect extends Effect<Offset> {
@@ -18,7 +18,7 @@ class MoveEffect extends Effect<Offset> {
           delay: delay,
           duration: duration,
           curve: curve,
-          begin: begin ?? const Offset(0, -16),
+          begin: begin ?? const Offset(0, -_defaultMove),
           end: end ?? Offset.zero,
         );
 
@@ -55,4 +55,42 @@ extension MoveEffectExtensions<T> on AnimateManager<T> {
         begin: begin,
         end: end,
       ));
+
+  /// Adds a `.moveX()` extension to [AnimateManager] ([Animate] and [AnimateList]).
+  /// This moves only on the x-axis according to the `double` begin/end values.
+  T moveX({
+    Duration? delay,
+    Duration? duration,
+    Curve? curve,
+    double? begin,
+    double? end,
+  }) =>
+      addEffect(MoveEffect(
+        delay: delay,
+        duration: duration,
+        curve: curve,
+        begin: Offset(begin ?? -_defaultMove, 0),
+        end: Offset(end ?? 0, 0),
+      ));
+
+  /// Adds a `.moveY()` extension to [AnimateManager] ([Animate] and [AnimateList]).
+  /// This moves only on the y-axis according to the `double` begin/end values.
+  T moveY({
+    Duration? delay,
+    Duration? duration,
+    Curve? curve,
+    double? begin,
+    double? end,
+  }) =>
+      addEffect(MoveEffect(
+        delay: delay,
+        duration: duration,
+        curve: curve,
+        begin: Offset(0, begin ?? -_defaultMove),
+        end: Offset(0, end ?? 0),
+      ));
+
+  // Note: there is no moveXY because diagonal movement isn't a significant use case.
 }
+
+const double _defaultMove = 16;
--- lib/effects/slide_effect.dart
+++ lib/effects/slide_effect.dart
@@ -5,7 +5,7 @@ import '../flutter_animate.dart';
 /// Effect that moves the target based on a fraction of its size (via [SlideTransition])
 /// based on the specified begin and end offsets. Defaults to `begin=Offset(0, -0.5),
 /// end=Offset.zero` (ie. slide down from half its height).
-/// 
+///
 /// To use pixel offsets instead, use [MoveEffect].
 @immutable
 class SlideEffect extends Effect<Offset> {
@@ -19,7 +19,7 @@ class SlideEffect extends Effect<Offset> {
           delay: delay,
           duration: duration,
           curve: curve,
-          begin: begin ?? const Offset(0, -0.5),
+          begin: begin ?? const Offset(0, -_defaultSlide),
           end: end ?? Offset.zero,
         );
 
@@ -53,4 +53,42 @@ extension SlideEffectExtensions<T> on AnimateManager<T> {
         begin: begin,
         end: end,
       ));
+
+  /// Adds a `.slideX()` extension to [AnimateManager] ([Animate] and [AnimateList]).
+  /// This slides only on the x-axis according to the `double` begin/end values.
+  T slideX({
+    Duration? delay,
+    Duration? duration,
+    Curve? curve,
+    double? begin,
+    double? end,
+  }) =>
+      addEffect(SlideEffect(
+        delay: delay,
+        duration: duration,
+        curve: curve,
+        begin: Offset(begin ?? -_defaultSlide, 0),
+        end: Offset(end ?? 0, 0),
+      ));
+
+  /// Adds a `.slideY()` extension to [AnimateManager] ([Animate] and [AnimateList]).
+  /// This slides only on the y-axis according to the `double` begin/end values.
+  T slideY({
+    Duration? delay,
+    Duration? duration,
+    Curve? curve,
+    double? begin,
+    double? end,
+  }) =>
+      addEffect(SlideEffect(
+        delay: delay,
+        duration: duration,
+        curve: curve,
+        begin: Offset(0, begin ?? -_defaultSlide),
+        end: Offset(0, end ?? 0),
+      ));
+
+  // Note: there is no slideXY because diagonal movement isn't a significant use case.
 }
+
+const double _defaultSlide = 0.5;
