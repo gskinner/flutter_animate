d3233793cacb77c013fd4cf0e9b825d273341b09
Shawn <shawn@gskinner.com>
2022-10-12 13:22:37 -0600

    Cleanup

--- test/effects/swap_test.dart
+++ test/effects/swap_test.dart
@@ -6,21 +6,22 @@ import '../tester_extensions.dart';
 
 void main() {
   testWidgets('swap Logo for a ColoredBox', (tester) async {
-    final basicSwapAnim = const FlutterLogo().animate().fadeOut(duration: 500.ms).swap(
+    final anim = const FlutterLogo().animate().fadeOut(duration: 500.ms).swap(
           // inherits duration from fadeOut
           builder: (_, __) => const ColoredBox(color: Colors.red).animate().fadeIn(),
         );
-    await tester.pumpAnimation(basicSwapAnim);
+    await tester.pumpAnimation(anim);
     expect(find.byType(FlutterLogo), findsOneWidget);
     await tester.pump(500.ms);
     await tester.pump(0.ms); // clear out the callback
     expect(find.byType(ColoredBox), findsOneWidget);
   });
+
   testWidgets('Fade a logo out and back in using swap()', (tester) async {
-    final fadeOutInAnim = const FlutterLogo().animate().fadeOut(duration: 500.ms).swap(
+    final anim = const FlutterLogo().animate().fadeOut(duration: 500.ms).swap(
           builder: (_, originalChild) => originalChild!.animate().fadeIn(),
         );
-    await tester.pumpAnimation(fadeOutInAnim);
+    await tester.pumpAnimation(anim);
     tester.expectWidgetValue<FadeTransition>((w) => w.opacity.value, 1, 'opacity');
     await tester.pump(500.ms);
     await tester.pump(0.ms);
