3400130dcb000b848e47cc19ef2feaafc0a555c0
himanshugarg08 <himanshu.dma@gmail.com>
2022-11-16 11:40:45 +0530

    updated to avoid chaining multiple getter calls

--- lib/extensions/num_duration_extensions.dart
+++ lib/extensions/num_duration_extensions.dart
@@ -8,9 +8,9 @@
 extension NumDurationExtensions on num {
   Duration get microseconds => Duration(microseconds: round());
   Duration get milliseconds => (this * 1000).microseconds;
-  Duration get seconds => (this * 1000).milliseconds;
-  Duration get minutes => (this * 60).seconds;
-  Duration get hours => (this * 60).minutes;
-  Duration get days => (this * 24).hours;
+  Duration get seconds => (this * 1000 * 1000).microseconds;
+  Duration get minutes => (this * 1000 * 1000 * 60).microseconds;
+  Duration get hours => (this * 1000 * 1000 * 60 * 60).microseconds;
+  Duration get days => (this * 1000 * 1000 * 60 * 60 * 24).microseconds;
   Duration get ms => milliseconds;
 }
