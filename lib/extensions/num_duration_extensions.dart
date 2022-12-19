b0d6e70b4594c711d43c6497bc5564e45b4db5df
Grant Skinner <grant@gskinner.com>
2022-12-18 17:11:13 -0700

    Improve code readability of num duration extension

--- lib/extensions/num_duration_extensions.dart
+++ lib/extensions/num_duration_extensions.dart
@@ -7,13 +7,10 @@
 /// ```
 extension NumDurationExtensions on num {
   Duration get microseconds => Duration(microseconds: round());
-  Duration get milliseconds => Duration(microseconds: (this * 1000).round());
-  Duration get seconds => Duration(microseconds: (this * 1000 * 1000).round());
-  Duration get minutes =>
-      Duration(microseconds: (this * 1000 * 1000 * 60).round());
-  Duration get hours =>
-      Duration(microseconds: (this * 1000 * 1000 * 60 * 60).round());
-  Duration get days =>
-      Duration(microseconds: (this * 1000 * 1000 * 60 * 60 * 24).round());
-  Duration get ms => milliseconds;
+  Duration get ms => (this * 1000).microseconds;
+  Duration get milliseconds => (this * 1000).microseconds;
+  Duration get seconds => (this * 1000 * 1000).microseconds;
+  Duration get minutes => (this * 1000 * 1000 * 60).microseconds;
+  Duration get hours => (this * 1000 * 1000 * 60 * 60).microseconds;
+  Duration get days => (this * 1000 * 1000 * 60 * 60 * 24).microseconds;
 }
