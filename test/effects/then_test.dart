48e30bac37a213765fb80d940d51efde9c182593
Grant Skinner <grant@gskinner.com>
2023-11-19 11:50:30 -0800

    Fix test impacted by ScaleEffect fix

--- test/effects/then_test.dart
+++ test/effects/then_test.dart
@@ -7,20 +7,20 @@ import '../tester_extensions.dart';
 void main() {
   testWidgets('ThenEffect: core', (tester) async {
     final animation =
-        const FlutterLogo().animate().fadeIn(duration: 500.ms).then().scale();
+        const FlutterLogo().animate().fadeIn(duration: 500.ms).then().scaleXY(begin: 0.50);
 
     // Wait 500ms, check that opacity has started, but scale has not
     await tester.pumpAnimation(animation, initialDelay: 250.ms);
     tester.expectWidgetWithDouble<FadeTransition>(
         (o) => o.opacity.value, 0.5, 'opacity @ 25%');
 
-    Matrix4 expectedMatrix = Transform.scale(scale: 0).transform;
+    Matrix4 expectedMatrix = Transform.scale(scale: 0.5).transform;
     tester.expectWidgetWithBool<Transform>(
-        (o) => o.transform == expectedMatrix, true, 'scale @ 25%');
+        (o) => o.transform == expectedMatrix, true, 'scale @ 50%');
 
     // Wait another 1s and check that scale is now halfway
     await tester.pumpAnimation(animation, initialDelay: 500.ms);
-    expectedMatrix = Transform.scale(scale: 0.5).transform;
+    expectedMatrix = Transform.scale(scale: 0.75).transform;
     tester.expectWidgetWithBool<Transform>(
         (o) => o.transform == expectedMatrix, true, 'scale @ 75%');
   });
